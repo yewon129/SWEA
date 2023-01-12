14d40810170c6302476268d9afa4b00f0b91ace0
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-01-12 19:35:03 +0900

    1/4 ps

--- 4866.py
+++ 4866.py
@@ -1,1 +1,27 @@
+T = int(input())
+for tc in range(1, T+1):
+  lst = map(str, input())
+  stack = []
+  flag = 1
 
+  for s in lst:
+    if s == '{':
+      stack.append('{')
+    elif s == '}':
+      if len(stack) != 0 and stack[-1] == '{':
+        stack.pop()
+      else:
+        flag = 0
+    elif s == '(':
+      stack.append('(')
+    elif s == ')':
+      if len(stack) != 0 and stack[-1] == '(':
+        stack.pop()
+      else:
+        flag = 0
+    if flag == 0:
+      break
+  if len(stack) != 0:
+    flag = 0
+
+  print(f'#{tc} {flag}')
