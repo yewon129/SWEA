ee5f5a32559fa0d4067b31dacc80ce8dbeb88c48
yewon129 <97646746+yewon129@users.noreply.github.com>
2023-01-12 19:21:44 +0900

    12/25 ps

--- 4835.py
+++ 4835.py
@@ -1,1 +1,18 @@
+T = int(input())
+for tc in range(1, T+1):
+  N, M = map(int, input().split())
+  lst = list(map(int, input().split()))
 
+  minV = 10000000
+  maxV = 0
+
+  for i in range(N-M+1):
+    cnt = 0
+    for j in range(M):
+      cnt += lst[i+j]
+    if cnt > maxV:
+      maxV = cnt
+    if cnt < minV:
+      minV = cnt
+  
+  print(f'#{tc} {maxV-minV}')
